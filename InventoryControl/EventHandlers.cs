b109f60e82c012fcca1b1c074a11c5ea5700584f
MrAfitol <mrafitol@gmail.com>
2022-12-23 07:18:45 +0200

    New version 1.0.3
    Ammo is now stored, but the main thing is not to forget to give the player armor

--- InventoryControl/EventHandlers.cs
+++ InventoryControl/EventHandlers.cs
@@ -5,8 +5,9 @@
     using PlayerRoles;
     using PluginAPI.Core;
     using PluginAPI.Core.Attributes;
-    using PluginAPI.Core.Items;
     using PluginAPI.Enums;
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
     using Random = UnityEngine.Random;
 
@@ -18,26 +19,59 @@
         {
             if (!InventoryControl.Instance.Config.Inventory.ContainsKey(newRole)) return;
 
-            Timing.CallDelayed(0.3f, () =>
+            Timing.CallDelayed(0.1f, () =>
             {
-                int itemCount = player.ReferenceHub.inventory.UserInventory.Items.Count;
-                int i = 0;
-
-                while (i < itemCount)
+                try
                 {
-                    player.ReferenceHub.inventory.ServerRemoveItem(player.ReferenceHub.inventory.UserInventory.Items.ElementAt(0).Key, null);
-                    i++;
-                }
+                    Dictionary<ItemType, ushort> Ammo = new Dictionary<ItemType, ushort>();
 
-                foreach (var RoleInventory in InventoryControl.Instance.Config.Inventory)
-                {
-                    if (RoleInventory.Key == newRole)
+                    foreach (var item in player.ReferenceHub.inventory.UserInventory.ReserveAmmo)
                     {
-                        foreach (var Item in RoleInventory.Value)
+                        Ammo.Add(item.Key, item.Value);
+                    }
+
+                    int ammoCount = player.ReferenceHub.inventory.UserInventory.ReserveAmmo.Count;
+                    int whileAmmo = 0;
+
+                    while (whileAmmo < ammoCount)
+                    {
+                        player.ReferenceHub.inventory.ServerSetAmmo(player.ReferenceHub.inventory.UserInventory.ReserveAmmo.ElementAt(whileAmmo).Key, 0);
+                        whileAmmo++;
+                    }
+
+                    int itemCount = player.ReferenceHub.inventory.UserInventory.Items.Count;
+                    int whileItem = 0;
+
+                    while (whileItem < itemCount)
+                    {
+                        player.ReferenceHub.inventory.ServerRemoveItem(player.ReferenceHub.inventory.UserInventory.Items.ElementAt(0).Key, null);
+                        whileItem++;
+                    }
+
+                    foreach (var RoleInventory in InventoryControl.Instance.Config.Inventory)
+                    {
+                        if (RoleInventory.Key == newRole)
                         {
-                            if (Item.Value >= Random.Range(0, 101)) player.ReferenceHub.inventory.ServerAddItem(Item.Key);
+                            foreach (var Item in RoleInventory.Value)
+                            {
+                                if (Item.Value >= Random.Range(0, 101)) player.ReferenceHub.inventory.ServerAddItem(Item.Key);
+                            }
                         }
                     }
+
+                    ammoCount = Ammo.Count;
+                    whileAmmo = 0;
+
+                    while (whileAmmo < ammoCount)
+                    {
+                        if (Ammo.ElementAt(whileAmmo).Value > 0)
+                            player.ReferenceHub.inventory.ServerSetAmmo(Ammo.ElementAt(whileAmmo).Key, Ammo.ElementAt(whileAmmo).Value);
+                        whileAmmo++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error("[InventoryControl] [Event: OnChangeRole] " + e.ToString());
                 }
             });
         }
